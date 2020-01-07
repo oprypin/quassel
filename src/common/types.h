bc898128c9ba15b0fdbf142fa17ced5a528d91e7
Manuel Nickschas <sputnick@quassel-irc.org>
2020-01-07 18:34:54 +0100

    common: Disable enum type stream operators for Qt >= 5.14
    Starting from version 5.14, Qt provides stream operators for enum
    types, which collide with the ones we ship in types.h. Disable
    Quassel's stream operators when compiling against Qt 5.14 or later.
    
    Add a unit test that ensures that enum serialization honors the width
    of the underlying type.

--- src/common/types.h
+++ src/common/types.h
@@ -140,6 +140,7 @@ Q_DECLARE_METATYPE(QHostAddress)
 typedef QList<MsgId> MsgIdList;
 typedef QList<BufferId> BufferIdList;
 
+#if QT_VERSION < QT_VERSION_CHECK(5, 14, 0)
 /**
  * Catch-all stream serialization operator for enum types.
  *
@@ -169,6 +170,7 @@ QDataStream &operator>>(QDataStream &in, T &value) {
     value = static_cast<T>(v);
     return in;
 }
+#endif
 
 // Exceptions
 
